d3eb7f57663aca6f97d2520f95f60dc9d49b7141
Yeolar <yeolar@gmail.com>
2018-05-30 11:52:08 +0800

    update cmake.

--- cmake-scripts/FindAccelerator.cmake
+++ cmake-scripts/FindAccelerator.cmake
@@ -7,18 +7,18 @@
 find_package(PkgConfig)
 pkg_check_modules(ACCELERATOR QUIET accelerator)
 
-IF (ACCELERATOR_INCLUDE_DIR)
+if(ACCELERATOR_INCLUDE_DIR)
   # Already in cache, be silent
-  SET(ACCELERATOR_FIND_QUIETLY TRUE)
-ENDIF ()
+  set(ACCELERATOR_FIND_QUIETLY TRUE)
+endif()
 
-FIND_PATH(ACCELERATOR_INCLUDE_DIR accelerator/accelerator-config.h)
+find_path(ACCELERATOR_INCLUDE_DIR accelerator/accelerator-config.h)
 
-FIND_LIBRARY(ACCELERATOR_LIBRARY accelerator)
+find_library(ACCELERATOR_LIBRARY accelerator)
 
 # handle the QUIETLY and REQUIRED arguments and set ACCELERATOR_FOUND to TRUE 
 # if all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(ACCELERATOR DEFAULT_MSG ACCELERATOR_LIBRARY ACCELERATOR_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ACCELERATOR DEFAULT_MSG ACCELERATOR_LIBRARY ACCELERATOR_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(ACCELERATOR_LIBRARY ACCELERATOR_INCLUDE_DIR)
+mark_as_advanced(ACCELERATOR_LIBRARY ACCELERATOR_INCLUDE_DIR)
